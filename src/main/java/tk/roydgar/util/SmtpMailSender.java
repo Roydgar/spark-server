a3ad208b62e0c423bbacdb986323162751ea9c8b
Viktor_Gritsaenko <Roydgaryshka@gmail.com>
2018-07-05 16:03:12 +0300

    Create gmail sender

---
+++ src/main/java/tk/roydgar/util/SmtpMailSender.java
@@ -0,0 +1,36 @@
+package tk.roydgar.util;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class SmtpMailSender {
+
+    private JavaMailSender javaMailSender;
+
+    public JavaMailSender send(String to, String subject, String body)  {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper;
+
+        try {
+            helper = new MimeMessageHelper(message, true);
+
+            helper.setSubject(subject);
+            helper.setTo(to);
+            helper.setText(body, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        javaMailSender.send(message);
+        return javaMailSender;
+    }
+}
+
