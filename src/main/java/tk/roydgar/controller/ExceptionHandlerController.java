cfa4e7809061210ab97f2c7e577be315c001eb09
Viktor_Gritsaenko <Roydgaryshka@gmail.com>
2018-08-08 15:57:48 +0300

    Edit messages to exception handler

--- src/main/java/tk/roydgar/controller/ExceptionHandlerController.java
+++ src/main/java/tk/roydgar/controller/ExceptionHandlerController.java
@@ -2,11 +2,22 @@ package tk.roydgar.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import tk.roydgar.util.HttpHeadersUtil;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Constraint;
+import javax.validation.ConstraintViolationException;
+
+import static tk.roydgar.util.HttpHeadersUtil.httpHeaders;
+import static tk.roydgar.util.constants.HeaderMessages.ERROR;
+import static tk.roydgar.util.constants.HeaderMessages.INTERNAL_ERROR;
+import static tk.roydgar.util.constants.HeaderMessages.INVALID_ID;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
@@ -15,15 +26,24 @@ public class ExceptionHandlerController {
     private Logger logger;
 
     @ExceptionHandler(NumberFormatException.class)
-    public String handleNumberCastException(HttpServletRequest request, Exception ex){
-        logger.error("Bad request [400]", ex);
-        return null;
+    public ResponseEntity<?> handleNumberCastException(HttpServletRequest request, Exception ex){
+        logger.error(ERROR, ex);
+        HttpHeaders httpHeaders = httpHeaders(ERROR, INVALID_ID);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Throwable.class)
-    public String handleGeneralException(HttpServletRequest request, Exception ex) {
-        logger.error("Exception", ex);
-        return null;
+    public ResponseEntity<?> handleGeneralException(HttpServletRequest request, Exception ex) {
+        logger.error(ERROR, ex);
+        HttpHeaders httpHeaders = httpHeaders(ERROR, INTERNAL_ERROR);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleValidationException(HttpServletRequest request, Exception ex) {
+        logger.error(ERROR, ex);
+        HttpHeaders httpHeaders = httpHeaders(ERROR, ex.getMessage());
+        return new ResponseEntity<>(httpHeaders, HttpStatus.BAD_REQUEST);
     }
 
 }
